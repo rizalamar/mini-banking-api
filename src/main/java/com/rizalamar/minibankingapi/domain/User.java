851c30356f4b2ef148917177a77f446e459b73fd
Rizal amarulloh <rizalamarulloh2014@gmail.com>
2026-06-04 21:36:46 +0700

    feat: implement abstract auditing entity and user entity

---
+++ src/main/java/com/rizalamar/minibankingapi/domain/User.java
@@ -0,0 +1,35 @@
+package com.rizalamar.minibankingapi.domain;
+
+import com.rizalamar.minibankingapi.domain.common.AbstractAuditingEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User extends AbstractAuditingEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String fullname;
+
+    @Builder.Default
+    private boolean isActive = true;
+}
