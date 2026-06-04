18837c02f4f2c8a8250f2cb67dc979399eeae1d7
Rizal amarulloh <rizalamarulloh2014@gmail.com>
2026-06-04 21:49:00 +0700

    fix: replace Data annotation wwith Getter and Setter

--- src/main/java/com/rizalamar/minibankingapi/domain/common/AbstractAuditingEntity.java
+++ src/main/java/com/rizalamar/minibankingapi/domain/common/AbstractAuditingEntity.java
@@ -5,13 +5,16 @@ import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
-@Data
+@Getter
+@Setter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class AbstractAuditingEntity {
