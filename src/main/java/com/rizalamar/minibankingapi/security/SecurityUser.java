25299c291f55b0d148ffc8fc3a8d75e9c5cf1664
Rizal amarulloh <rizalamarulloh2014@gmail.com>
2026-06-04 22:33:55 +0700

    feat: implement security user details and custom user details service and fix: pom.xml by remove version in security starter

---
+++ src/main/java/com/rizalamar/minibankingapi/security/SecurityUser.java
@@ -0,0 +1,55 @@
+package com.rizalamar.minibankingapi.security;
+
+import com.rizalamar.minibankingapi.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class SecurityUser implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.isActive();
+    }
+
+    public User getUser(){
+        return user;
+    }
+}
---
+++ src/main/java/com/rizalamar/minibankingapi/security/UserDetailsServiceImpl.java
@@ -0,0 +1,22 @@
+package com.rizalamar.minibankingapi.security;
+
+import com.rizalamar.minibankingapi.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .map(SecurityUser::new)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+    }
+}
