f732e862417e0bad671d66c994956fc91e374923
Rizal amarulloh <rizalamarulloh2014@gmail.com>
2026-06-04 20:44:39 +0700

    setup project

---
+++ src/main/java/com/rizalamar/minibankingapi/MiniBankingApiApplication.java
@@ -0,0 +1,13 @@
+package com.rizalamar.minibankingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniBankingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniBankingApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rizalamar/minibankingapi/MiniBankingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rizalamar.minibankingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniBankingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
