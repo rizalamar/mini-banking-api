11bf1fa47de8cf9dbdaa645eb6b5cd853fd20b7b
Rizal amarulloh <rizalamarulloh2014@gmail.com>
2026-06-06 14:26:41 +0700

    feat: implement user response

---
+++ src/main/java/com/rizalamar/minibankingapi/dto/user/UserResponse.java
@@ -0,0 +1,19 @@
+package com.rizalamar.minibankingapi.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserResponse {
+    private UUID id;
+    private String email;
+    private String fullName;
+    private String role;
+}
